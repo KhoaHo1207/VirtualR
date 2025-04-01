c200927446ec253c5d5be76a0c69b7d2da752545
Khoa <khoaho120703@gmail.com>
2025-04-01 11:12:39 +0700

    [Add]_Workflow

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import viteLogo from "/vite.svg";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <FeatureSection />
+        <Workflow />
       </div>
     </>
   );
